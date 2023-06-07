8f4c45166eff5996cf901895d2eab40ad91bbfc8
ZizoAlzeeka <128798645+ZizoAlzeeka@users.noreply.github.com>
2023-06-08 02:19:23 +0300

    Update index.php

--- math/index.php
+++ math/index.php
@@ -1,62 +1,5 @@
 
 
-<?php
-session_start();
-/*if(isset($_SESSION['user'])){
-    header('location:profile.php');
-    exit();
-}*/
-if(isset($_POST['submit'])){
- include 'conn-db.php';
-   $password=filter_var($_POST['password'],FILTER_SANITIZE_STRING);
-   $name=filter_var($_POST['name'],FILTER_SANITIZE_STRING);
-  
-
-   $errors=[];
-   
-
-  
-
-
-   // validate password
-   if(empty($password)){
-        $errors[]="يجب كتابة  كلمة المرور ";
-   }
-
-
-
-   // insert or errros 
-   if(empty($errors)){
-   
-    
-        
-		$stm="SELECT * FROM users WHERE password ='$password'";
-		$q=$conn->prepare($stm);
-		$q->execute();
-        $data=$q->fetch(); 
-        $_POST['name']='';
-         
-        if(!$data){
-			$errors[] = "خطأ فى تسجيل الدخول";
-         }
-         
-         else{
-
-
-            $_SESSION['user']=[
-                "name"=>$name,
-               
-                
-              ];
-              header('location:main.php');
-         }
-     
-    
-}
-}
-?>
-
-
 
 
 <!DOCTYPE HTML>
@@ -65,21 +8,44 @@ if(isset($_POST['submit'])){
 <!-- Mirrored from mazn.co/introtops/login.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 17 Mar 2023 20:26:10 GMT -->
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>تصميم رقمي   | for you &#x2665;</title>
+<title> مقرر التفاضل   | for you &#x2665;</title>
 
-<meta property="og:description" content="(شرح مقرر تصميم رقمي ( شروحات + حل تمارين + واجبات ">
-<meta property="og:title"content="(شرح مقرر تصميم رقمي ( شروحات + حل تمارين + واجبات ">
+<meta property="og:description" content="(شرح مقرر التفاضل  ( شروحات + حل تمارين + واجبات ">
+<meta property="og:title"content="(شرح مقرر التفاضل  ( شروحات + حل تمارين + واجبات ">
 
 
 
-<meta name="twitter:description" content="  شرح مقرر تصميم رقمي  شروحات + حل تمارين + واجبات ">
+<meta name="twitter:description" content="  شرح مقرر التفاضل   شروحات + حل تمارين + واجبات ">
 
 <link rel="stylesheet" href="assets/css/maind28d.css?v=1.8" />
 <link rel="stylesheet" href="../../cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/css/noscript.css" />
 <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 <script src="assets/js/sweetalert2.all.min.js"></script>
 <style>
+    .logo4 {
+text-align: center;
+margin: 20px auto;
+color:#000;
+background-color: #FFF; 
+padding: 10px;
+position: relative;
+border-radius: 7px;
+-webkit-border-radius: 7px;
+-moz-border-radius: 7px;
+font-size: 1rem !important;
+font-weight: bold;
+-webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+}
+
+.logo4 {width: 98%;
+}
+
+
+
 			.swal2-styled {
 				margin: .3125em;
     			padding: 0em 77px;
@@ -125,16 +91,136 @@ input[type="checkbox"] + label, input[type="radio"] + label {
     -o-transition: all 0.5s ease;
     transition: all 0.5s ease;
 }
+
+
+/*-------------------------------*/
+.container{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: darkslategray;
+
+
+}
+
+.btn-send{
+    border: 0;
+    background: white;
+    border-radius: 50px;
+    padding: 10px 60px;
+    cursor: pointer;
+
+}
+.card{
+    background: rgb(255, 253, 161);
+    border-radius: 20px;
+    box-shadow: 1px 1px 1px 1px black;
+    padding: 1em;
+    position: absolute;
+    top:-100%;
+    left: 50%;
+    text-align: center;
+    width: 90%;
+    transform: translate(-50%,-50%);
+    transition: all .5s;
+    opacity: 0;
+}
+
+.card.open{
+    opacity: 1;
+    top: 180%;
+    transition-property: top;
+  transition-duration: 0.5s;
+  transition-delay: 0.5s;
+}
+
+.card.close{
+    opacity: 0;
+    top: -50%;
+    
+}
+.card img{
+    width: 300px;
+    height: 300px;
+    border-radius: 20px;
+    margin-top: -150px;
+
+}
+
+.card h2{
+    font-size: 50px;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+.card .btn-close{
+width: 100%;
+border-radius: 50px;
+border: 0px;
+padding: 15px;
+margin-top: 20px;
+background: aquamarine;
+color: white;
+cursor: pointer;
+
+}
+
+.Aara{
+    background: white;
+    border-radius: 20px;
+    text-align: center;
+}
+.Aara h2{
+    color: #000;
+    padding: 10px;
+   
+}
+.Aara details{
+    background: #431f8d;
+    margin: auto;
+    width: 90%;
+    padding: 10px;
+    border-radius: 20px;
+    color: white;
+    font-weight: bold;
+	font-size:50px
+    
+   
+    
+}
+.Aara details ul{
+
+    height: 500px;
+    background: #7d56cd;
+    overflow: scroll;
+}
+.Aara details img{
+    margin: 0px;
+    width: 50%;
+    margin: 10px auto;
+border-radius: 20px;
+}
+
+
+
+
+
+
+
 		</style>
 </head>
 <body class="is-preload">
-
+	<div style="margin-top: 20px;text-align: center;"><img style="border-radius: 20px;height: 200px;width: 80%;border: 0px;box-shadow: 1px 1px 1px 1px black;" src="https://media.tenor.com/2pB1ng_3qQsAAAAC/welcome.gif" alt="">
+</div><br>
+<div style="text-align: center;margin-top: 20px;"><img style="border-radius: 20px;height: 200px;width: 400px;border: 0px;box-shadow: 1px 1px 1px 1px black;" src="https://getlogovector.com/wp-content/uploads/2019/08/kingdom-of-saudi-arabia-vision-2030-logo-vector.png" alt="">
+</div>
 <div id="wrapper">
 
 <header id="header" class="alt">
 <h1 class="logo">    &#x2665;   for you    &#x2665;</h1>
-<h1>تصميم رقمي</h1>
-<h4>شرح مقرر تصميم رقمي( شروحات + حل تمارين + واجبات )</h4><br><hr>
+<h1>التفاضل </h1>
+<h4>شرح مقرر التفاضل ( شروحات + حل تمارين + واجبات )</h4><br><hr>
 </header>
 
 
@@ -143,25 +229,67 @@ input[type="checkbox"] + label, input[type="radio"] + label {
 
 
         <section class="main special">
-            <header class="major">
-            <h2>شرح تجريبي</h2>
+               
+           
+               <!-- <center><marquee scrollamount="5" direction="right" ><b style="background: white;padding: 10px;color: red;font-size: 20px;border: solid black 1px;border-radius: 20px;font-weight: bold;">
+                   [ موقع فوريو يقدم لكم ايضا شرح المقررات التالية  [  التفاضل والتكامل 1 و 2   + مقدمة في الرياضيات  + جبر خطي + معادلات تفاضلية + نظرية مجموعات + تحليل عددي + رياضيات متقطعة + الفيزياء 
+                <b></marquee> <br>
+                </center>-->
+            
+                
+           <!-- <marquee  style="background: white;padding: 10px;color: red;font-size: 20px;border: solid black 1px;border-radius: 20px;font-weight: bold;">الاشس علاعسشي هعغلشعسي سي شسبيثصهخع يسبهتعهسيب بيثخهثب ثبثبهخ </marquee>
+        --><header class="major">
+		<img src="../images/ALAMERI2.jpg" alt="" width="100%">
+		 <h2>د / منور العامري </h2>
+            <h2>شرح تجريبي لمقرر التفاضل</h2>
             </header>
-            <iframe width="560" height="315" style="max-width:94%;" src="https://www.youtube.com/embed/YHFhmaIu43k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <br><hr>
-            <ul class="list">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/29rsXhqHkH0" style="max-width:94%;" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
+        
+        <br><hr>
+
+        <header class="major">
+        <h2>شرح تجريبي لمقرر جبر خطي</h2>
+    </header>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/uAEwkcvOITQ" style="max-width:94%;" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<br><hr>
+
+
+
+
+		<ul class="list">
                 <summary>يتضمن هذا الموقع </summary><hr>
                 
                 <li style="padding:10px;">شرح المقرر بالكامل &#x2713;</li>
                 <li  style="padding:10px;">حل الواجبات أولا بأول &#x2713;</li>
+                <li  style="padding:10px;">متابعة لاي سؤال او استفسار&#x2713;</li>
                 <li  style="padding:10px;">خدمات أخرى &#x2713;</li>
                
                 <li></li>
             </ul><hr>
-            <p class="price" style="padding:10px;">سعر الإشتراك شامل كل شروحات المقرر مع الواجبات ب <span>(   299 ريال) <br> بدلا من<del style="color: red;"> 350 ريال  </del> </span> <br>ويمديك تدفع اي وقت  (الآن او وقت المكافأة ) </p>
+            <p class="price" style="padding:10px;">سعر الإشتراك شامل كل شروحات المقرر مع الواجبات ب <span>(   200 ريال) <br> بدلا من<del style="color: red;"> 250 ريال  </del> </span> <br>ويمديك تدفع اي وقت  (الآن او وقت المكافأة ) </p>
 
-            							
+            <hr>
+            <header class="major">
+                <h2> كما يقدم لكم موقع فوريو ايضا شروحات للمقررات التالية <br> <i class="fa fa-arrow-circle-down" style="font-size:48px;color:red"></i></h2>
+                </header>
+                <div align="center"  class="logo4">
+                <font color="#D4002E"><marquee scrollamount="3" direction="right" bgcolor="#FFFFFF">
+                    <b style="color:
+                    red;font-weight: bold;">       التفاضل والتكامل 1 و 2   + مقدمة في الرياضيات  + جبر خطي + معادلات تفاضلية <hr style="color: red;">+ نظرية مجموعات + تحليل عددي + رياضيات متقطعة + الفيزياء  + التصميم المنطقي الرقمي.......................
+                    </b></marquee> <br>
+            
+                </font>
+                </div>							
             </section>
         </section></div><br><hr><br>
+
+
+
+
+
+        
+
+
 <div id="main">       
 <section id="content" class="main">
 
@@ -173,55 +301,112 @@ input[type="checkbox"] + label, input[type="radio"] + label {
 <header class="major">
 <h2>تسجيل الدخول</h2>
 </header>
-<form method="post" action="#">
+<br><br>
 
 
-<?php 
-							if(isset($errors)){
-								if(!empty($errors)){
-									foreach($errors as $msg){
-										echo $msg . "<br>";
-									}
-								}
-							}
-						?>
-<div class="row gtr-uniform">
 
-<div class="col-12 col-12-xsmall inputt">
-    <input type="text" name="name" id="MZPassword"  placeholder="اسمك" required>
-    </div>
-<div class="col-12 col-12-xsmall inputt">
-<input type="password" name="password" id="MZPassword" placeholder="كلمة المرور" required>
+
+
+
+
+<div class="col-12 ">
+
+
+<!--<li style="display: block;width: 88%;margin: auto;" onclick="show_pup()" ><a href="#"  target="_blank" class="button primary fit " style="display: block;width: 90%;">طلب اشتراك جديد</a></li>
+-->
+<img src="http://static.skaip.org/img/emoticons/180x180/f6fcff/pointdownindex.gif" alt=""><br>
+<button style="display: block;margin: auto;width: 90%;font-weight: bold;font-size: 30px;" class="button primary fit "  onclick="show_pup()"> طلب اشتراك جديد</button>
+<div class="card" id="pup">
+    <img src="https://icon-library.com/images/emoji-icon-png/emoji-icon-png-1.jpg" alt="">
+    <h2>هلا والله</h2>
+    <p><span style="font-size: 50px;font-wight:bold;" >للحصول على الخصم قم بنسخ هذا الكود (<strong style="color:
+        red;font-size: 50px;"> Zforyou </strong>) وارساله لنا عبر الضغط على إشتراك   </span></p>
+    <!--<button class="btn-close">إرسال </button>-->
+    <a href="https://wa.me/qr/6YQT2SXQYVV6O1" style="display: block;margin: auto;width: 90%;background: #431f8d;color: white;padding:10px;border-radius: 20px;font-weight: bold;font-size: 30px;">إشتراك</a><br>
+    <button style="display: block;margin: auto;width: 90%;background: #431f8d;color: white;padding:10px;border-radius: 20px;font-weight: bold;text-align: center;font-size: 30px;" class="button primary fit "  onclick="hiad_pup()"> إالغاء  </button>
+</div>
+</div></section></section>
 </div>
 
-<div class="col-12">
-<input type="checkbox" id="agree-human" name="agree-human" required>
-<label for="agree-human">أتعهد وأقسم بالله بأني لن أقوم بتسريب أو إعادة نشر أي محتوى من محتويات الموقع .</label>
-</div>
-<div class="col-12">
+<br><br><hr>
 
-<li style="display: block;width: 80%;margin: auto;"><input type="submit" value="تسجيل دخول" name="submit" id="login" class="button primary fit disabled log" ></li><br>
-<li style="display: block;width: 88%;margin: auto;" ><a href="https://wa.me/qr/23PJHZ6DTHWRP1" target="_blank" class="button primary fit " style="display: block;width: 90%;">طلب اشتراك جديد</a></li>
+<div class="Aara ">
+    <h2>درجات الطلاب المشتركين معنا </h2>
+    
+    <br><br>
 
-</div>
-</div>
-</form>
-</section>
+    <details>
+        <summary> <i class="fa fa-hand-o-down" style="font-size:30px;color:red"></i> عرض </summary>
+        <ul>
+            <li><img src="../images/grad/grad11.jpg" alt=""></li>
+            <li><img src="../images/grad/grad1.jpg" alt=""></li>
+            <li><img src="../images/grad/grad2.jpg" alt=""></li>
+            <li><img src="../images/grad/grad3.jpg" alt=""></li>
+            <li><img src="../images/grad/grad4.jpg" alt=""></li>
+            <li><img src="../images/grad/grad5.jpg" alt=""></li>
+            <li><img src="../images/grad/grad6.jpg" alt=""></li>
+            <li><img src="../images/grad/grad7.jpg" alt=""></li>
+            <li><img src="../images/grad/grad8.jpg" alt=""></li>
+            <li><img src="../images/grad/grad9.jpg" alt=""></li>
+            <li><img src="../images/grad/grad10.jpg" alt=""></li>
+            
+        </ul>
+    </details>
+    <br><br><br>
+    <h2>آراء الطلاب المشتركين معنا </h2>
+    
+    <br><br>
 
-</section>
+    <details>
+        <summary> <i class="fa fa-hand-o-down" style="font-size:30px;color:red"></i> عرض </summary>
+        <ul>
+            <li><img src="../images/araa/A1.jpg" alt=""></li>
+            <li><img src="../images/araa/A2.jpg" alt=""></li>
+            <li><img src="../images/araa/A3.jpg" alt=""></li>
+            <li><img src="../images/araa/A4.jpg" alt=""></li>
+            <li><img src="../images/araa/A5.jpg" alt=""></li>
+            <li><img src="../images/araa/A6.jpg" alt=""></li>
+            
+            
+        </ul>
+    </details>
+    <br><br>
+   
+
+    
+    
+</div>
+
+
 </div>
 
 <footer id="footer">
 <ul class="icons copyright">
-<li><a href="https://wa.me/qr/23PJHZ6DTHWRP1" class="icon brands fa-whatsapp alt"><span class="label">whatsapp</span></a></li>
-<li><a href="https://instagram.com/itsmaz1n" class="icon brands fa-telegram alt"><span class="label">Instagram</span></a></li>
-<li><a href="https://snapchat.com/add/itsMaz1n" class="icon brands fa-snapchat alt"><span class="label">Snapchat</span></a></li>
-</ul>
+<li><a href="https://wa.me/qr/6YQT2SXQYVV6O1" class="icon brands fa-whatsapp alt"><span class="label">whatsapp</span></a></li>
+<li><a href="https://t.me/bmonaw" class="icon brands fa-telegram alt"><span class="label">telegram</span></a></li>
 
-<li class="copyright">&copy;  جميع الحقوق محفوظة ل<a  href="https://wa.me/qr/23PJHZ6DTHWRP1" style="text-decoration: none;"> ALZEEKA</a></li>
+</ul><br>
+<!--<li class="copyright"><a href="../index.html"  style="font-size:20px;color:yellow;margin: auto;" >موقع خاص لشرح مادة التصميم المنطقي الرقمي</a></li>-->
+
+<li class="copyright">&copy;  جميع الحقوق محفوظة لموقع<a  href="index.html" style="text-decoration: none;"> FOR <i class="fa fa-heart" style="font-size:20px;color:red"></i> YOU </a></li>
 										
 </footer>
 </div>
+<script>
+    function show_pup(){
+        document.getElementById('pup').classList.add('open');
+        document.getElementById('pup').classList.remove('close');
+
+
+    }
+
+    function hiad_pup(){
+        document.getElementById('pup').classList.add('close');
+        document.getElementById('pup').classList.remove('open');
+
+
+    }
+</script>
 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/jquery.scrollex.min.js"></script>
